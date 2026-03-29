e41e893aea7336ec8c3ae6d0aeb793396300216b
aashneelc <aashneelc@gmail.com>
2026-03-28 22:32:44 -0700

    Update lab_1b.py

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -7,7 +7,19 @@ then performs the operation and returns the result.
 The script asks the user to input the numbers and the operation to be performed,
 and prints the result to the terminal window.
 
+test2
+test3
+test4
+test5
+test6
+test7
 """
+def request_sanitized_number(prompt: str) -> float:
+    while True:
+        try:
+            return float(input(prompt))
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
 
 def simple_calculator(operation: str, num1: float, num2: float) -> float:
     """
